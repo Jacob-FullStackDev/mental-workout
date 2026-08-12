6fed6348fd74ef7a16964be7e439e5b0e074d0f7
JacobLuber2 <jaluber10@gmail.com>
2026-08-12 14:11:06 -0600

    Select DOM elements

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 "use strict";
-const preboardingHeading = document.getElementById("preboarding-heading");
+// DOM ELEMENT SELECTORS
+const preboardingHeadingEl = document.getElementById("preboarding-heading");
+const durationInputEl = document.getElementById("session-duration");
+const goalInputEl = document.getElementById("session-goal");
+const firstOperandInputEl = document.getElementById("first-operand-length");
+const secondOperandInputEl = document.getElementById("second-operand-length");
+const firstOperandSampleEl = document.getElementById("sample-operand--1");
+const secondOperandSampleEl = document.getElementById("sample-operand--2");
+const startSessionEl = document.getElementById("start-session");
+const mathProblemEl = document.getElementById("math-problem");
+const sessionCountdownEl = document.getElementById("session-countdown");
+const accuracyEl = document.getElementById("accuracy");
+
 let mode = "multiplication";
 preboardingHeading.textContent += ` ${mode}`;
