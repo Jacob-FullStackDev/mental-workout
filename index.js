1317240ec90fba37f2a6c0f3a8eb005c670f06da
JacobLuber2 <jaluber10@gmail.com>
2026-08-13 17:56:46 -0600

    Finish sample problem generation feature

--- index.js
+++ index.js
@@ -9,30 +9,82 @@ const sampleProblemEl = document.getElementById("sample-problem");
 const startSessionEl = document.getElementById("start-session");
 const sessionCountdownEl = document.getElementById("session-countdown");
 const accuracyEl = document.getElementById("accuracy");
-const sampleProblemOperand1El = document.getElementById("sample-operand-1");
-const sampleProblemOperand2El = document.getElementById("sample-operand-2");
-const sampleProblemOperatorEl = document.getElementById("sample-operator");
+const firstOperandOfSampleProblemEl =
+  document.getElementById("sample-operand-1");
+const secondOperandOfSampleProblemEl =
+  document.getElementById("sample-operand-2");
+const sampleProblemFeedbackEl = document.getElementById(
+  "sample-problem-feedback",
+);
 // INITIAL STATE
 let sessionInProgress = false;
 let sampleProblemDisplayed = false;
 let sampleProblem = ` × `;
 function startSession(firstLength, secondLength = firstLength) {
   sessionInProgress = true;
-  let firstOperand = Math.round((Math.random() * 10 + 1) ** firstLength);
-  let secondOperand = Math.round((Math.random() * 10 + 1) ** secondLength);
 }
 
 function operandGen(digits) {
-  let max = 10 ** digits - 1;
-  let min = 10 ** (digits - 1) - 1;
-  let operand = Math.round(Math.random() * (max - min)) + min;
-  return operand;
+  if (digits >= 1) {
+    let max = 10 ** digits - 1;
+    let min = 10 ** (digits - 1) - 1;
+    let operand = Math.floor(Math.random() * (max - min)) + min + 1;
+    return operand;
+  }
 }
+
 firstOperandInputEl.addEventListener("input", (e) => {
   e.preventDefault();
-  sampleProblemOperand1El.textContent = operandGen(Number(e.target.value));
+  const digitsLength = Number(e.target.value);
+  if (digitsLength >= 1 && digitsLength <= 15) {
+    firstOperandOfSampleProblemEl.textContent = operandGen(digitsLength);
+  }
+  if (!secondOperandInputEl.value) {
+    secondOperandOfSampleProblemEl.textContent = operandGen(
+      firstOperandOfSampleProblemEl.textContent.length,
+    );
+  }
+  if (!firstOperandInputEl.value) {
+    // value removed
+    if (secondOperandInputEl.value) {
+      // use length of other operand
+      firstOperandOfSampleProblemEl.textContent = operandGen(
+        secondOperandOfSampleProblemEl.textContent.length,
+      );
+    } else if (!secondOperandInputEl.value) {
+      secondOperandOfSampleProblemEl.textContent =
+        firstOperandOfSampleProblemEl.textContent = "";
+      sampleProblemEl.classList.add("hidden");
+      return;
+    }
+  }
+  sampleProblemEl.classList.remove("hidden");
 });
+
 secondOperandInputEl.addEventListener("input", (e) => {
   e.preventDefault();
-  sampleProblemOperand1El.textContent = operandGen(Number(e.target.value));
+  const digitsLength = Number(e.target.value);
+  if (digitsLength >= 1 && digitsLength <= 15) {
+    secondOperandOfSampleProblemEl.textContent = operandGen(digitsLength);
+  }
+  if (!firstOperandInputEl.value) {
+    firstOperandOfSampleProblemEl.textContent = operandGen(
+      secondOperandOfSampleProblemEl.textContent.length,
+    );
+  }
+  if (!secondOperandInputEl.value) {
+    // value removed
+    if (firstOperandInputEl.value) {
+      // use length of other operand
+      secondOperandOfSampleProblemEl.textContent = operandGen(
+        firstOperandOfSampleProblemEl.textContent.length,
+      );
+    } else if (!firstOperandInputEl.value) {
+      firstOperandOfSampleProblemEl.textContent =
+        secondOperandOfSampleProblemEl.textContent = "";
+      sampleProblemEl.classList.add("hidden");
+      return;
+    }
+  }
+  sampleProblemEl.classList.remove("hidden");
 });
